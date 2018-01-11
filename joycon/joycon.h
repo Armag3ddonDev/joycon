3917a19dae24cf94ccd16fbe8f7f8fabd2f00329
Silvan Lindner <silvan.lindner@gmail.com>
2018-01-11 20:17:06 +0100

    added signal handling; added THROW/CHECK; added JoyconVec; refractured code

--- joycon/joycon.h
+++ joycon/joycon.h
@@ -1,12 +1,23 @@
 #pragma once
 
-#include "hidapi/hidapi.h"
+#include <thread>
+#include <vector>
+
+#ifdef _WIN32
+#include "hidapi.h"
+#elif __linux__
+#include <hidapi/hidapi.h>
+#endif
+
+#include <string>
+#include <stdexcept>
+#define THROW(x) throw(std::runtime_error(std::string(__FILE__) + " - line " + std::to_string(__LINE__) + ": " + __FUNCTION__ + "(): " + x ))
+#define CHECK(x) if (x == -1) {THROW(#x + " failed!");}
 
 #define MAX_STR 255
-
 #define JOYCON_VENDOR 0x057e
 
-enum JOY_TYPE {
+enum JOY_PID {
 	JOYCON_L_BT = 0x2006,
 	JOYCON_R_BT = 0x2007,
 	PRO_CONTROLLER = 0x2009,
@@ -18,18 +29,24 @@ public:
 
 	Joycon(Joycon&) = delete;
 	Joycon(Joycon&&) = default;
-	Joycon(JOY_TYPE type, wchar_t* serial_number);
+	Joycon(JOY_PID PID, wchar_t* serial_number);
 
 	~Joycon();
 
+	void printDeviceInfo() const;
 	void send_command(unsigned char cmd, unsigned char subcmd, std::vector<unsigned char> data);
-
 	void capture();
 	void callback();
 
 private:
-	hid_device * handle;
+	hid_device* handle;
 	std::thread callback_thread;
 	bool alive = true;
 	std::size_t package_number = 0;
+};
+
+class JoyconVec : public std::vector<Joycon> {
+public:
+	int addDevices();
+	int startDevices();
 };
