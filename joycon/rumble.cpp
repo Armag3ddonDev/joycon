2174a7060df474a106ae0a876fd3462d1dc4d59b
Silvan Lindner <silvan.lindner@gmail.com>
2018-01-18 20:35:53 +0100

    finished rumble!

--- joycon/rumble.cpp
+++ joycon/rumble.cpp
@@ -3,69 +3,58 @@
 #include "types.h"
 #include "rumble.h"
 
-/*
 void Rumble::pack() {
 
-	bool offset = false;
-	if (type == JOYCON::LEFT) {
-		offset = false;
-	}
-	else if (type == JOYCON::RIGHT) {
-		offset = true;
-	}
-	else {
-		throw std::invalid_argument("Invalid JOYCON type.");
-	}
+	byte hf;
+	byte lf;
+	byte hf_amp;
+	byte lf_amp;
 
-	uint16_t hf;
-	uint8_t lf;
-	uint8_t hf_amp;
-	uint16_t lf_amp;
+	encode_frequency(this->frequency, hf, lf);
+	encode_amplitude(this->amplitude, hf_amp, lf_amp);
 
-	encode_frequency(frequency, hf, lf);
-	encode_amplitude(amplitude, hf_amp, lf_amp);
-
-	//Byte swapping
-	buf[2 + offset * 4] = hf & 0xFF;
-	buf[3 + offset * 4] = hf_amp + ((hf >> 8) & 0xFF); //Add amp + 1st byte of frequency to amplitude byte
-													   //Byte swapping
-	buf[4 + offset * 4] = lf + ((lf_amp >> 8) & 0xFF); //Add freq + 1st byte of LF amplitude to the frequency byte
-	buf[5 + offset * 4] = lf_amp & 0xFF;
+	this->data[0] = (hf << 2);
+	this->data[1] = (hf_amp << 1) | (hf >> 6);
+	this->data[2] = (lf_amp << 7) | lf;
+	this->data[3] = 0x40 | (lf_amp >> 1);
 }
-*/
 
 void Rumble::unpack() {
+	byte hf     = (data[1] << 6) | (data[0] >> 2);
+	byte hf_amp = data[1] >> 1;
+	byte lf     = (data[2] << 1) >> 1;
+	byte lf_amp = ((data[3] << 2) >> 1) | (data[2] >> 7);
 
+	this->frequency = decode_frequency(hf, lf);
+	this->amplitude = decode_amplitude(hf_amp, lf_amp);
 }
 
-void Rumble::encode_frequency(double frequency, uint16_t& hf, uint8_t& lf) const {
+void Rumble::encode_frequency(double frequency, byte& hf, byte& lf) const {
 
 	// Float frequency to hex conversion
-	if (frequency < 40.87) {
-		frequency = 40.87;
-	}
-	else if (frequency > 1252.57) {
-		frequency = 1252.57;
+	if (frequency < 40.87 || frequency > 1252.57) {
+		throw std::invalid_argument("frequency must be between 40.87 and 1252.57.");
 	}
 
 	// maps to 0x41(65) - 0xDF(223)
-	uint8_t encoded_hex_freq = static_cast<uint8_t>(std::round(std::log2(frequency / 10.0)*32.0));
+	byte encoded_hex_freq = static_cast<byte>(std::round(std::log2(frequency / 10.0)*32.0));
 
-	// Convert to Joy-Con HF range. Range in big-endian: 0x0004(81.75Hz)-0x01FC(1252.57Hz) with +0x0004 steps.
-	hf = (encoded_hex_freq - 0x60) * 4;	// what happens under 81.75Hz?
-										// Convert to Joy-Con LF range. Range: 0x01-0x7F.
-	lf = encoded_hex_freq - 0x40;
+	// Convert to Joy-Con HF range. Range: 0x01. - 0x7F
+	hf = (encoded_hex_freq > 0x60) ? (encoded_hex_freq - 0x60) : 0x00;	// what happens under 81.75Hz?
+
+	// Convert to Joy-Con LF range. Range: 0x01-0x7F.
+	lf = (encoded_hex_freq < 0xC0) ? encoded_hex_freq - 0x40 : 0x00;
 }
 
-void Rumble::encode_amplitude(double amplitude, uint8_t& hf_amp, uint16_t& lf_amp) const {
+void Rumble::encode_amplitude(double amplitude, byte& hf_amp, byte& lf_amp) const {
 
-	// dont increase for safety-reasons of the linear resonant actuators. max safe is 1.002867
+	// dont increase for safety-reasons of the linear resonant actuators. max safe is 1.002867; real max is 1.799701
 	if ((amplitude < 0.0) || (amplitude > 1.0)) {
 		throw std::invalid_argument("amplitude must be between 0 and 1.");
 	}
 
-	// maps to 0x00(0) - 0xC8(100)
-	uint8_t amp_encoded;
+	// maps to 0x00(0) - 0x64(100) [safe] | 0x7E(126) [unsafe]
+	byte amp_encoded;
 	if (amplitude < 0.008) {
 		amp_encoded = 0x00;
 	}
@@ -80,36 +69,37 @@ void Rumble::encode_amplitude(double amplitude, uint8_t& hf_amp, uint16_t& lf_am
 	}
 
 	// saveguard
-	if (amp_encoded > 0xC8) {
+	if (amp_encoded > 0x64) {
 		throw std::bad_exception();
 	}
 
-	hf_amp = amp_encoded << 1;	// multiply by 2
-	lf_amp = 0x8000 * (amp_encoded & 0x01) + 0x40 + (amp_encoded >> 1);	// 0x80XX if odd, else 0x00XX
+	hf_amp = amp_encoded;
+	lf_amp = amp_encoded;
 }
 
-double Rumble::decode_frequency(uint16_t hf, uint8_t lf) const {
+double Rumble::decode_frequency(byte hf, byte lf) const {
 
 	uint8_t encoded_hex_freq = lf + 0x40;
-	if (encoded_hex_freq != ((hf >> 2) + 0x60)) {
+	if (encoded_hex_freq != hf + 0x60) {
 		throw std::runtime_error("lf (" + std::to_string(lf) + ") and hf (" + std::to_string(hf) + ") produce different frequencies!");;
+	} else if (encoded_hex_freq > 0xDF){
+		throw std::runtime_error("frequency must be between 0x00 and 0xDF.");
 	}
 
-	return 10.0*std::pow(2.0, static_cast<float>(encoded_hex_freq) / 32.0);
+	double frequency =  10.0*std::pow(2.0, static_cast<float>(encoded_hex_freq) / 32.0);
+
+	return std::min(40.87, std::min(1252.57, frequency));
 }
 
-double Rumble::decode_amplitude(uint8_t hf_amp, uint16_t lf_amp) const {
+double Rumble::decode_amplitude(byte hf_amp, byte lf_amp) const {
 
-	uint8_t amp_encoded = hf_amp >> 1;
-	if (lf_amp != (0x8000 * (amp_encoded & 0x01) + 0x40 + (amp_encoded >> 1))) {
+	if (hf_amp != lf_amp) {
 		throw std::runtime_error("lf_amp (" + std::to_string(lf_amp) + ") and hf_amp (" + std::to_string(hf_amp) + ") produce different frequencies!");;
+	} else if (hf_amp > 0x64) {
+		throw std::runtime_error("amplitude must be between 0x00 and 0x64.");
 	}
 
-	// saveguard
-	if (amp_encoded > 0xC8) {
-		throw std::bad_exception();
-	}
-
+	uint8_t amp_encoded = hf_amp;
 	double amplitude;
 
 	// maps to 0 - 1.00295
@@ -126,9 +116,5 @@ double Rumble::decode_amplitude(uint8_t hf_amp, uint16_t lf_amp) const {
 		amplitude = 18.0 / 5.0*std::pow(2.0, static_cast<double>(amp_encoded + 1) / 32.0 - 5.0);
 	}
 
-	// amplitude of 0xC8 is 1.002948 because of 'std::round' during encoding
-	if ((amplitude < 0.0) || (amplitude > 1.002948)) {
-		throw std::runtime_error("Something went wrong. Amplitude should be between 0 and 1.");
-	}
 	return std::max(amplitude, 1.0);
 }
--- joycon/rumble.h
+++ joycon/rumble.h
@@ -4,6 +4,33 @@
 
 #include "types.h"
 
+// data: 01LL LLLL | KIII IIII | JJJJ JJJG | HHHH HH00
+
+/* ORIGINAL */
+// ID		START	STEP	END			Byte
+// hf:		0x0004	0x0004	0x01FC		0000 000G HHHH HH00
+// lf:		0x01	0x01	0x7F		          0III IIII
+// hf_amp:	0x00	0x02	0xFE		          JJJJ JJJ0
+// lf_amp:	0xX040	0x0001	0xX07F		K000 0000 01LL LLLL		// where X alternates between 8 and 0
+
+/* ALTERNATIVE <--- USED */ 
+// ID		START	STEP	END		Byte
+// hf:		0x01	0x01	0x7F	0GHH HHHH
+// lf:		0x01	0x01	0x7F	0III IIII
+// hf_amp:	0x01	0x01	0x7F	0JJJ JJJJ
+// lf_amp:	0x01	0x01	0x7F	0LLL LLLK
+// -> valid if any < 0x80
+
+// data[0] = (hf << 2);
+// data[1] = (hf_amp << 1) | (hf >> 6);
+// data[2] = (lf_amp << 7) | lf;
+// data[3] = 0x40 | (lf_amp >> 1);
+
+// hf     = (data[1] << 6) | (data[0] >> 2);
+// hf_amp = data[1] >> 1;
+// lf     = (data[2] << 1) >> 1;
+// lf_amp = ((data[3] << 2) >> 1) | (data[2] >> 7);
+
 class Rumble
 {
 public:
@@ -14,27 +41,26 @@ public:
 		pack();
 	}
 
-	Rumble(ByteArray<4> data) : frequency(0.0), amplitude(0.0),  data(data) {
+	Rumble(const ByteArray<4>& data) : frequency(0.0), amplitude(0.0),  data(data) {
 		unpack();
 	}
 
 	const double& getFreqeuncy() const { return frequency; }
 	const double& getAmplitude() const { return amplitude; }
-	const unsigned char& byte_at(std::size_t idx) const { return data.at(idx); }
+	const byte& byte_at(std::size_t idx) const { return data.at(idx); }
 
 private:
 
 	void pack();	// frequency + amplitude -> data (4 byte)
 	void unpack();	// frequency + amplitude <- data (4 byte)
 
-	void encode_frequency(double frequency, uint16_t& hf, uint8_t& lf) const;
-	void encode_amplitude(double amplitude, uint8_t& hf_amp, uint16_t& lf_amp) const;
+	void encode_frequency(double frequency, byte& hf, byte& lf) const;
+	void encode_amplitude(double amplitude, byte& hf_amp, byte& lf_amp) const;
 
-	double decode_frequency(uint16_t hf, uint8_t lf) const;
-	double decode_amplitude(uint8_t hf_amp, uint16_t lf_amp) const;
+	double decode_frequency(byte hf, byte lf) const;
+	double decode_amplitude(byte hf_amp, byte lf_amp) const;
 
 	double frequency;
 	double amplitude;
 	ByteArray<4> data{0x00, 0x01, 0x40, 0x40};
 };
-
