af05862b0d35cc5f2b87bd31b1c93524645bc51f
al3xst <keks@al3xst.de>
2018-02-05 13:54:32 +0100

    Changed appveyor to new msys2 configuration

---
+++ ci/appveyor_build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+pacman -Syuu --noconfirm
+pacman -S --needed --noconfirm $MINGW_PACKAGE_PREFIX-{cmake, ninja, qtcreator, toolchain, hidapi}
+
+PROJECTPATH=$(cygpath ${APPVEYOR_BUILD_FOLDER})
+
+mkdir build
+cd build
+cmake -G Ninja $PROJECT_PATH
+
+ninja -j2 #build
+
+ninja test #run tests
